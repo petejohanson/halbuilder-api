3e5a569613925ef16b359e24e255f82b242587da
Mark Derricutt <mark.derricutt@smxemail.com>
2014-03-30 20:14:38 +1300

    Initial updates for SourceRepresentations
    Now - when reading a representation from a Reader you end up with a
    SourceRepresentation which adds a getContent() method giving you access
    to the original content that was downloaded.

--- src/main/java/com/theoryinpractise/halbuilder/api/RepresentationFactory.java
+++ src/main/java/com/theoryinpractise/halbuilder/api/RepresentationFactory.java
@@ -6,39 +6,40 @@ import java.net.URISyntaxException;
 import java.util.Set;
 
 public abstract class RepresentationFactory {
-    public static final String HAL_XML = "application/hal+xml";
-    public static final String HAL_JSON = "application/hal+json";
+  public static final String HAL_XML = "application/hal+xml";
 
-    public static final URI PRETTY_PRINT = makeUri("urn:halbuilder:prettyprint");
+  public static final String HAL_JSON = "application/hal+json";
 
-    public static final URI COALESCE_LINKS = makeUri("urn:halbuilder:coalescelinks");
+  public static final URI PRETTY_PRINT = makeUri("urn:halbuilder:prettyprint");
 
-    public static final URI STRIP_NULLS = makeUri("urn:halbuilder:stripnulls");
-    
-    public static final URI SINGLE_ELEM_ARRAYS = makeUri("urn:halbuild:singleelemarrays");
+  public static final URI COALESCE_LINKS = makeUri("urn:halbuilder:coalescelinks");
 
-    protected static URI makeUri(String uri) {
-        try {
-            return new URI(uri);
-        } catch (URISyntaxException e) {
-            throw new RepresentationException(e);
-        }
+  public static final URI STRIP_NULLS = makeUri("urn:halbuilder:stripnulls");
+
+  public static final URI SINGLE_ELEM_ARRAYS = makeUri("urn:halbuild:singleelemarrays");
+
+  protected static URI makeUri(String uri) {
+    try {
+      return new URI(uri);
+    } catch (URISyntaxException e) {
+      throw new RepresentationException(e);
     }
+  }
 
-    public abstract RepresentationFactory withNamespace(String namespace, String href);
+  public abstract RepresentationFactory withNamespace(String namespace, String href);
 
-    public abstract RepresentationFactory withLink(String rel, String href);
+  public abstract RepresentationFactory withLink(String rel, String href);
 
-    public abstract RepresentationFactory withFlag(URI flag);
+  public abstract RepresentationFactory withFlag(URI flag);
 
-    public abstract Representation newRepresentation(URI uri);
+  public abstract Representation newRepresentation(URI uri);
 
-    public abstract Representation newRepresentation();
+  public abstract Representation newRepresentation();
 
-    public abstract Representation newRepresentation(String href);
+  public abstract Representation newRepresentation(String href);
 
-    public abstract ReadableRepresentation readRepresentation(String contentType, Reader reader);
+  public abstract SourceRepresentation readRepresentation(String contentType, Reader reader);
 
-    public abstract Set<URI> getFlags();
+  public abstract Set<URI> getFlags();
 
 }
